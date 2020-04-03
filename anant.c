e69102bc5d38d37ad1c24e333220aa4de8f62441
= <=>
2020-04-03 21:01:32 +0530

    "Date fixed."

--- anant.c
+++ anant.c
@@ -10,22 +10,33 @@ void new_acc()
     FILE *ptr;
 
     ptr = fopen("record.dat", "a+");
-account_no:
-    system("cls");
-    printf("\t\t\t ADD RECORD  ");
-    printf("\n\n\nEnter today's date(mm/dd/yyyy):");
-    scanf("%d/%d/%d", &add.deposit.month, &add.deposit.day, &add.deposit.year);
-    printf("\nEnter the account number:");
-    scanf("%d", &check.acc_no);
-    while (fscanf(ptr, "%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d\n", &add.acc_no, add.name, &add.dob.month, &add.dob.day, &add.dob.year, &add.age, add.address, add.citizenship, &add.phone, add.acc_type, &add.amt, &add.deposit.month, &add.deposit.day, &add.deposit.year) != EOF)
-    {
-        if (check.acc_no == add.acc_no)
+    
+    account_no:
+        system("cls");
+        printf("\t\t\t ADD RECORD  ");
+        
+        SYSTEMTIME t; 
+        GetLocalTime(&t);
+
+        printf("\n\n\n Today's date (DD/MM/YYYY): %d/%d/%d", t.wDay, t.wMonth, t.wYear);
+        
+        add.deposit.day = t.wDay; 
+        add.deposit.month = t.wMonth;
+        add.deposit.year = t.wYear;
+        
+        printf("\nEnter the account number:");
+        scanf("%d", &check.acc_no);
+        
+        while (fscanf(ptr, "%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d\n", &add.acc_no, add.name, &add.dob.month, &add.dob.day, &add.dob.year, &add.age, add.address, add.citizenship, &add.phone, add.acc_type, &add.amt, &add.deposit.month, &add.deposit.day, &add.deposit.year) != EOF)
         {
-            printf("Account no. already in use!");
-            fordelay(1000000000);
-            goto account_no;
+            if (check.acc_no == add.acc_no)
+            {
+                printf("Account no. already in use. Try again with a different account number.");
+                fordelay(1000000000);
+                goto account_no;
+            }
         }
-    }
+
     add.acc_no = check.acc_no;
     printf("\nEnter the name:");
     scanf("%s", add.name);
