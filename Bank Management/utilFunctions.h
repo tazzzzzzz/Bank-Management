51b6175a821dc292e2ab6a153b6e708c6e0f0591
tazzzzzzz <47104651+tazzzzzzz@users.noreply.github.com>
2020-05-07 02:37:38 +0530

    remove

--- Bank Management/utilFunctions.h
+++
@@ -1,8 +0,0 @@
-void start(); //Done
-void menu();    //Done
-
-void fordelay(int j);   //Done
-int findAge(int current_date, int current_month, int current_year, int birth_date, int birth_month, int birth_year); //Done
-int passwordAuthentication();
-
-float calcEMI(float principal, float ratepercent_per_annum, int installments);
