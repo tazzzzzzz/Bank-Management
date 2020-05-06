774cf2777a7707ac35beea56db93d0203cf5d5d6
tazzzzzzz <47104651+tazzzzzzz@users.noreply.github.com>
2020-05-06 14:02:38 +0530

    Remove Whitespaces for citizenship, address Inputs

--- Bank Management/inputValidation.c
+++ Bank Management/inputValidation.c
@@ -54,6 +54,16 @@ float getFloat(){
         goto getFloatValue;
 }
 
+void removeSpaces(char *str) 
+{ 
+    int count = 0; 
+  
+    for (int i = 0; str[i]; i++) 
+        if (str[i] != ' ') 
+            str[count++] = str[i]; 
+    str[count] = '\0'; 
+} 
+
 /*
     EXP00-A. Use parentheses for precedence of operation
 
